0104658ad3117fb7e190c4076ab06c08f17884c3
Nick Papoutsis <izybit@gmail.com>
2017-09-28 19:26:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,12 +13,20 @@ function yourAnimal() {
   return animal
 }
 
-function add2(n) {
-  return n + two
 
+
+
+
+
+
+function add2(n) {
   // Feel free to move things around!
-  const two = 2
+  var two = 2;
+  return two + n;
 }
+
+
+
 
 var funkyFunction = function() {
   return function() {
@@ -28,4 +36,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()();
+theFunk;
